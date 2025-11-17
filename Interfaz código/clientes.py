58087744e6053fa1748c0a252566818d8fea7136
Estrella Gomez <go544913@uaeh.edu.mx>
2025-11-16 21:19:58 -0600

    Código de interfaz de la ventana de clientes.py

--- Interfaz código/clientes.py
+++ Interfaz código/clientes.py
@@ -11,6 +11,6 @@ for campo in campos:
     tk.Label(root, text=campo + ":", font=("Arial", 12)).pack()
     tk.Entry(root, width=40).pack(pady=5)
 
-tk.Button(root, text="Enviar", bg="black", fg="white", width=15).pack(pady=10)
+tk.Button(root, text="Enviar", bg="#FFCBDB", fg="black", width=15).pack(pady=10)
 
 root.mainloop()
