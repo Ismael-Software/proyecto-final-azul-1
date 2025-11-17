8b810edf591d480a05c17f20fcd1c4aa3d90aa40
Estrella Gomez <go544913@uaeh.edu.mx>
2025-11-16 22:18:38 -0600

    Código de interfaz de la ventana de Menu.py

--- Interfaz código/Menu.py
+++ Interfaz código/Menu.py
@@ -2,14 +2,21 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("JEZNAY - Menú Principal")
-root.geometry("600x500")
+root.geometry("700x550")
 root.configure(bg="#f7f7f7")
 
-tk.Label(root, text="JEZNAY", font=("Arial", 26, "bold"), fg="#c9302c", bg="#f7f7f7").pack(pady=30)
+tk.Label(root, text="JEZNAY", font=("Arial", 26, "bold"), fg="#c9302c", bg="#f7f7f7").pack()
 tk.Label(root, text="Menú Principal", font=("Arial", 14), bg="#f7f7f7").pack(pady=10)
 
 botones = ["Productos", "Ventas", "Clientes", "Proveedor", "Empleados"]
 for texto in botones:
-    tk.Button(root, text=texto, bg="#e74c3c", fg="white", width=20, height=3).pack(pady=5)
+    tk.Button(
+        root,
+        text=texto,
+        bg="#ffcbdb",
+        fg="black",
+        width=20,
+        height=3
+    ).pack(pady=5)
 
 root.mainloop()
