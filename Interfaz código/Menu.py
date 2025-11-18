b8574f0dd0e79c1cc36f0ae1c34f3c247239c575
Estrella Gomez <go544913@uaeh.edu.mx>
2025-11-18 17:11:22 -0600

    new line

--- Interfaz código/Menu.py
+++ Interfaz código/Menu.py
@@ -12,6 +12,8 @@ root.title("JEZNAY - Menú Principal")
 root.geometry("600x550")
 root.configure(bg="#f7f7f7")
 
+
+
 tk.Label(root, text="JEZNAY", font=("Arial", 26, "bold"), fg="#c9302c", bg="#f7f7f7").pack(pady=30)
 tk.Label(root, text="Menú Principal", font=("Arial", 14), bg="#f7f7f7").pack(pady=10)
 
