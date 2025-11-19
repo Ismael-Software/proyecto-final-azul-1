38cdaf9e8c05ab7232e062cf8866d184ec7e644e
José María <sa546357@uaeh.edu.mx>
2025-11-19 14:28:40 -0600

    Código la interfaz Clientes, hecho por Estrella

--- Interfaz código/clientes.py
+++ Interfaz código/clientes.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from BD.clientesConsulta import clientesConsulta
 
 root = tk.Tk()
 root.title("JEZNAY - Clientes")
@@ -6,11 +7,27 @@ root.geometry("401x401")
 
 tk.Label(root, text="Clientes", font=("Arial", 20, "bold")).pack(pady=15)
 
-campos = ["ID Cliente", "Nombre", "Teléfono/Celular"]
-for campo in campos:
-    tk.Label(root, text=campo + ":", font=("Arial", 12)).pack()
-    tk.Entry(root, width=40).pack(pady=5)
+tk.Label(root, text="Id Cliente:", font=("Arial", 12)).pack()
+vaIDCliente = tk.Entry(root, width=40)
+vaIDCliente.pack(pady=5)
 
-tk.Button(root, text="Enviar", bg="#FFCBDB", fg="black", width=15).pack(pady=10)
+tk.Label(root, text="Nombre:", font=("Arial", 12)).pack()
+vaNombre = tk.Entry(root, width=40)
+vaNombre.pack(pady=5)
+
+tk.Label(root, text="Telefono:", font=("Arial", 12)).pack()
+vaTelefono = tk.Entry(root, width=40)
+vaTelefono.pack(pady=5)
+
+def invocarBoton():
+    x = vaIDCliente.get()
+    y = vaNombre.get()
+    z = vaTelefono.get()
+
+    cc = clientesConsulta()
+    cc.conexion()
+    cc.insertar(x, y, z)
+
+tk.Button(root, text="Enviar", bg="#FFCBDB", fg="black", width=15, command=invocarBoton).pack(pady=10)
 
 root.mainloop()
